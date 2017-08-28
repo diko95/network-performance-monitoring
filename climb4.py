fac0867c720fc37eca6329e4339a2c50deed83e3
diko95 <31407645+diko95@users.noreply.github.com>
2017-08-28 15:02:09 +0200

    Create climb4.py

---
+++ climb4.py
@@ -0,0 +1,24 @@
+import os
+import json
+import urllib2
+
+'''proc = subprocess.Popen('', stdout=subprocess.PIPE)
+output = proc.stdout.read()
+print output'''
+ 
+sudoPassword = 'diko'
+command = 'tshark -i ens33 -T fields -e ip.addr -e tcp.dstport -e tcp.srcport -e tcp.analysis.ack_rtt -e frame.time_epoch -E aggregator=/s '
+p = os.system('echo %s|sudo -S %s' % (sudoPassword, command))
+
+
+'''data = {
+        'username': 'diko',
+        'password':'diko'
+}
+
+req = urllib2.Request('http://localhost:5555/login')
+req.add_header('Content-Type', 'application/json')
+
+response = urllib2.urlopen(req, json.dumps(data))'''
+
+#curl -i -H "Content-Type: application/json" -X POST -d '{"username":"nikhil","password":"mara"}' http://localhost:5000/login
